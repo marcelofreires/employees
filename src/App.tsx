61b75152c1bf99105fcbe937f3330c6617d5a2d6
marcelofreires <marcelo.freire.silva@gmail.com>
2024-03-21 16:37:57 -0300

    chore: add mui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Button from '@mui/material/Button'
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,8 +8,12 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function handleCounter() {
+    setCount((count) => count + 1)
+  }
+
   return (
-    <>
+    <main>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -19,9 +24,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button onClick={handleCounter} variant="outlined">count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +32,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </main>
   )
 }
 
