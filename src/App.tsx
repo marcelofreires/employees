6f3697d7e7352a66af898054176d035da57ffa2c
marcelofreires <marcelo.freire.silva@gmail.com>
2024-03-22 01:57:18 -0300

    feat: use AppHeader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,57 @@
-import { useState } from 'react'
-import Button from '@mui/material/Button'
+import { Container, Typography, Paper, Grid, styled, Box } from '@mui/material'
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AppHeader } from './components';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.grey[200],
+  height: 300,
+  width: '100%',
+  display: 'block',
+}));
+
+function arr(numOfItems = 6) {
+  return Array.from(Array(numOfItems))
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  function handleCounter() {
-    setCount((count) => count + 1)
-  }
-
   return (
-    <main>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={handleCounter} variant="outlined">count is {count}</Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </main>
+    <>
+      <AppHeader />
+      <Box
+        component="main"
+        py={{ xs: 4, sm: 8 }}
+      >
+        <Container>
+          <Box>
+            <Typography
+              component="h2"
+              variant="h4"
+              mb={4}
+            >
+              Funcionários
+            </Typography>
+          </Box>
+          <Box>
+            <Grid
+              container
+              spacing={6}
+            >
+              {arr(20).map((_, index) => (
+                <Grid
+                  item
+                  key={index}
+                  xs={12}
+                  sm={6}
+                  md={4}
+                >
+                  <Item />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Container>
+      </Box>
+    </>
   )
 }
 
