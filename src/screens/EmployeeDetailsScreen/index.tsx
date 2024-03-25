58a66abcf5ff73f604e505531371c623f57449d0
marcelofreires <marcelo.freire.silva@gmail.com>
2024-03-25 19:11:36 -0300

    feat: add loading on employee details page

--- src/screens/EmployeeDetailsScreen/index.tsx
+++ src/screens/EmployeeDetailsScreen/index.tsx
@@ -7,15 +7,26 @@ import { BaseScreenLayout } from '../layouts/BaseScreenLayout'
 import type { Employee } from 'src/types'
 import { employeeService } from 'src/services/employeeService'
 import { EmployeeForm } from './components'
+import { Loading } from 'src/components'
 
 export function EmployeesDetailsScreen() {
-  const [employee, setEmployee] = useState<Employee>()
+  const [employee, setEmployee] = useState<Employee | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
   const { id } = useParams<{ id: Employee['id'] }>()
 
   async function handleGetEmployee(id: string) {
-    const response = await employeeService.getEmployee(id)
 
-    setEmployee(response)
+
+    try {
+      setIsLoading(true)
+      const response = await employeeService.getEmployee(id)
+
+      setEmployee(response)
+    } catch (error) {
+      setEmployee(null)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   useEffect(() => {
@@ -31,6 +42,7 @@ export function EmployeesDetailsScreen() {
           <Button startIcon={<ArrowBackIcon />} component="span">Voltar</Button>
         </Link>
       </Box>
+      { isLoading && <Loading isLoading={isLoading} /> }
       {employee && (
         <>
           <Typography component="h2" variant="h4" mb={4}>{employee?.name}</Typography>
